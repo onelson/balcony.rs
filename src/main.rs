2f222666eb1385de812cf37259c3fb85f0a04bc6
Owen Nelson <onelson@gmail.com>
2018-01-22 01:11:38 -0800

    read from a stream uri as a first arg to the binary

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
 extern crate opencv;
 
+use std::env;
 use opencv::core;
+use opencv::highgui;
+
+fn run(url: &str) -> Result<(),String> {
+    let window = "video capture";
+    try!(highgui::named_window(window,1));
+    let mut cam = try!(highgui::VideoCapture::new(url));
+
+    loop {
+        let mut frame = try!(core::Mat::new());
+        try!(cam.read(&mut frame));
+        if try!(frame.size()).width > 0 {
+            try!(highgui::imshow(window, &mut frame));
+        }
+        if try!(highgui::wait_key(10)) > 0 {
+            break;
+        }
+    }
+    Ok(())
+}
 
 fn main() {
-    println!("{}", core::get_build_information().unwrap())
+    let stream_url = env::args().skip(1).next().unwrap();
+    println!("{}", stream_url);
+    run(&stream_url).unwrap();
 }
