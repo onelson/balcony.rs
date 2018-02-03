8e785a625afe223af4b4510db325c964cd89cd1a
Owen Nelson <onelson@gmail.com>
2018-02-03 13:48:04 -0800

    WIP port of the java template match example
    Added a stub of `min_max_loc()` which is not included in in the opencv
    bindings (perhaps because it was 3rd party, and vendored into the lib?)
    
    Will attempt to port from the C++
    https://github.com/opencv/opencv/blob/05b15943d6a42c99e5f921b7dbaa8323f3c042c6/3rdparty/carotene/src/minmaxloc.cpp

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ extern crate opencv;
 use std::env;
 use opencv::core;
 use opencv::highgui;
+use opencv::imgproc;
 
-fn run(url: &str) -> Result<(),String> {
+fn run_viewer(url: &str) -> Result<(),String> {
     let mut cam = highgui::VideoCapture::new(url)?;
 
     if !cam.is_opened()? {
@@ -29,8 +30,87 @@ fn run(url: &str) -> Result<(),String> {
     }
 }
 
-fn main() {
+fn main2() {
     let stream_url = env::args().skip(1).next().unwrap();
     println!("{}", stream_url);
-    run(&stream_url).unwrap();
+    run_viewer(&stream_url).unwrap();
+}
+
+
+// ----------------
+
+//import org.opencv.core.Core;
+//import org.opencv.core.Core.MinMaxLocResult;
+//import org.opencv.core.CvType;
+//import org.opencv.core.Mat;
+//import org.opencv.core.Point;
+//import org.opencv.core.Scalar;
+//import org.opencv.highgui.Highgui;
+//import org.opencv.imgproc.Imgproc;
+
+
+struct MinMaxLocResult { min_loc: core::Point, max_loc: core::Point }
+
+
+fn min_max_loc(mat: &core::Mat) -> MinMaxLocResult {
+    unimplemented!();
+}
+
+
+fn run(in_file: &str, template_file: &str, out_file: &str, match_method: i32) -> Result<(), String> {
+    println!("Running Template Matching");
+    let img: core::Mat = highgui::imread(in_file, 0)?;
+    let templ: core::Mat = highgui::imread(template_file, 0)?;
+
+    let tp_size = templ.size()?;
+
+    // / Create the result matrix
+    let result: core::Mat = {
+        let im_size = img.size()?;
+        let result_cols: i32 = im_size.width - tp_size.width + 1;
+        let result_rows: i32 = im_size.height - tp_size.height + 1;
+        core::Mat::new_rows_cols(result_rows, result_cols, core::CV_32FC1)?
+    };
+
+    // / Do the Matching and Normalize
+    imgproc::match_template(&img, &templ, &result, match_method)?;
+    core::normalize(&result, &result, 0.0, 1.0, core::NORM_MINMAX, -1, &core::Mat::new()?)?;
+
+    // / Localizing the best match with minMaxLoc
+    let mmr = min_max_loc(&result);
+
+    let match_loc =
+        if match_method == imgproc::TM_SQDIFF || match_method == imgproc::TM_SQDIFF_NORMED {
+            mmr.min_loc
+        } else {
+            mmr.max_loc
+        };
+
+    // / Show me what you got
+    core::rectangle(
+        &img,
+        core::Rect {
+            x: match_loc.x,
+            y: match_loc.y,
+            width: tp_size.width,
+            height: tp_size.height
+        },
+        core::Scalar { data: [0.0, 255.0, 0.0, 1.0] },
+        1,
+        8,
+        0
+    )?;
+
+    // Save the visualized detection.
+    println!("Writing {}", out_file);
+    let params = opencv::types::VectorOfint::new();
+    highgui::imwrite(out_file, &img, &params)?;
+
+    Ok(())
+
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    run(&args[0], &args[1], &args[2], imgproc::TM_CCOEFF).unwrap();
 }
